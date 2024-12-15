b3810451b451b71103f93af4f8b99a6a183e69c3
spunkyboy <120793467+spunkyboy@users.noreply.github.com>
2024-12-15 09:36:04 +0000

    Add files via upload

--- preloader.js
+++ preloader.js
@@ -1,8 +1,19 @@
-window.addEventListener('load', function() {
-    // Hide the preloader and show the main content after the page is fully loaded
-    const preloader = document.getElementById('preloader');
-    const content = document.getElementById('content');
-    
-    preloader.style.display = 'none';
-    content.style.display = 'block';
-});
+// Wait for the DOM content to be loaded
+document.addEventListener("DOMContentLoaded", function() {
+    const image = document.querySelector(".preload-image");
+    const preloader = document.querySelector(".image-preloader");
+  
+    // Add an event listener for when the image has loaded
+    image.onload = function() {
+      // Add the 'loaded' class to display the image
+      image.classList.add("loaded");
+      // Hide the preloader
+      preloader.style.display = "none";
+    };
+  
+    // If the image is already cached, manually trigger the load event
+    if (image.complete) {
+      image.onload(); // Trigger onload if the image is already loaded
+    }
+  });
+  
